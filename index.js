38dfe5abde34a894e3e15199ab7c733d0dd20f4f
Hidekazu Saegusa <hidekazu.saegusa@frogdesign.com>
2017-11-08 23:27:47 -0800

    multi channel virtual midi

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
 var navigator = require('web-midi-api');
 
-navigator.requestMIDIAccess().then(onMIDISuccess,onMIDIFailure);
+navigator.requestMIDIAccess()
+         .then(onMIDISuccess, onMIDIFailure);
 
 var midi = null;
 var inputs = [];
 var outputs = [];
 
 function onMIDISuccess(m){
-  console.log('成功')
-  console.log(m, 'm?')
+  // console.log('成功')
+  // console.log(m, 'm?')
   midi = m;
+
   var it = midi.inputs.values();
   for(var o = it.next(); !o.done; o = it.next()){
     inputs.push(o.value);
   }
-  var ot = midi.outputs.values();
-  for(var o = ot.next(); !o.done; o = ot.next()){
-    outputs.push(o.value);
-  }
 
-  for(var cnt=0;cnt < inputs.length;cnt++){
-    inputs[cnt].onmidimessage = onMIDIEvent;
-  }
-}
+  for(var i = 0; i < inputs.length; i++){
+    inputs[i].onmidimessage = onMIDIEvent;
+    const deviceName = inputs[i].name;
 
-function onMIDIFailure(msg){
-  console.log("onMIDIFailure()呼ばれただと？:"+msg);
+    inputs[i].onmidimessage = (e) => {
+      console.log(e, 'event');
+      console.log(deviceName, 'channel');
+    };
+  }
 }
 
 function onMIDIEvent(e){
-  if(e.data[2] != 0){
-    // なにかをうけとったときの処理
-    console.log(e.data[2]);
-  }
+  console.log(e, 'event fired');
 }
 
-function sendMIDINoteOn(note){
-  if(outputs.length > 0){
-    outputs[0].send([0x90,note,0x7f]);
-  }
+function onMIDIFailure(msg){
+  console.log('error called...', msg);
 }
